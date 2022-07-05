54e70299b514bad15fc171714f3554331fdd3f17
ITSC <leedh7785@naver.com>
2022-07-05 12:08:50 +0900

    commit

--- jstl/src/main/java/test/TestServlet.java
+++ jstl/src/main/java/test/TestServlet.java
@@ -45,6 +45,9 @@ public class TestServlet extends HttpServlet {
 		list.add(new MemberDTO("A0002", "1234", "BBBB", 20, "남", 33333, "Bronze"));
 		list.add(new MemberDTO("A0003", "1234", "CCCC", 20, "남", 33333, "Silver"));
 		
+		request.setAttribute("list", list);
+		request.setAttribute("age", 20);
+		request.getRequestDispatcher("jstl_el.jsp").forward(request, response);	
 	}
 
 	/**

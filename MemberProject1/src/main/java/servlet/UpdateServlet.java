8797bc8e452dbd9a91cd4a141f6ea2dd77075529
ITSC <leedh7785@naver.com>
2022-07-06 15:06:50 +0900

    commit

--- MemberProject1/src/main/java/servlet/UpdateServlet.java
+++ MemberProject1/src/main/java/servlet/UpdateServlet.java
@@ -6,6 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.MemberDAO;
+import dto.MemberDTO;
 
 /**
  * Servlet implementation class UpdateServlet
@@ -26,6 +30,24 @@ public class UpdateServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 수정된 정보들을 전부 가져옴
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+		String name = request.getParameter("name");
+		int age = Integer.parseInt(request.getParameter("age"));
+		String gender = request.getParameter("gender");
+		String address = request.getParameter("address");
+		MemberDTO dto = new MemberDTO(id, passwd, name, age, gender, address);
+		
+		int result = MemberDAO.getInstance().updateMemberDTO(dto);
+		System.out.println(result + "건 수정이 완료됨");
+		HttpSession session = request.getSession();
+		dto.setPass(null);
+		session.setAttribute("dto", dto);
+		
+		
+		response.sendRedirect("main.jsp");
+		
 		
 	}
 
@@ -33,7 +55,7 @@ public class UpdateServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
 		doGet(request, response);
 	}
 

822d6568c15ba69f9b042ed43167390fc829d6c0
ITSC <leedh7785@naver.com>
2022-07-11 15:25:33 +0900

    commit

---
+++ Board/src/main/java/controller/MainController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dto.BoardDTO;
+import service.BoardService;
+import view.ModelAndView;
+
+public class MainController implements Controller {
+
+	@Override
+	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int pageNo = 1;
+		
+		if(request.getParameter("pageNo") != null) {
+			pageNo = Integer.parseInt(request.getParameter("pageNo"));
+		}
+		
+		ArrayList<BoardDTO> list = BoardService.getInstance().selectBoardList(pageNo);
+		request.setAttribute("board_list", list);
+		
+		ModelAndView view = new ModelAndView("main.jsp", false);
+		
+		return view;
+	}
+
+}
--- Board/src/main/java/service/BoardService.java
+++ Board/src/main/java/service/BoardService.java
@@ -1,8 +1,11 @@
 package service;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
 import config.DBManager;
+import dao.BoardDAO;
+import dto.BoardDTO;
 
 public class BoardService {
 	private static BoardService instance = new BoardService();
@@ -16,6 +19,10 @@ public class BoardService {
 		if(instance == null)
 			instance = new BoardService();
 		return instance;
+	}
+
+	public ArrayList<BoardDTO> selectBoardList(int pageNo) {
+		return BoardDAO.getInstance().selectBoardList(pageNo);
 	}
 	
 	

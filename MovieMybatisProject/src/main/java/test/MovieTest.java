5ff8ecccf4164e5d643e634b5aa66ce6f867fce9
ITSC <leedh7785@naver.com>
2022-07-20 11:55:57 +0900

    commit

--- MovieMybatisProject/src/main/java/test/MovieTest.java
+++ MovieMybatisProject/src/main/java/test/MovieTest.java
@@ -5,15 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+import org.apache.ibatis.session.SqlSession;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import config.DBManager;
 import dto.MovieDTO;
 
 class MovieTest {
+	static SqlSession session;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -28,18 +31,23 @@ class MovieTest {
 		}
 		System.out.println(str);
 		JSONArray arr = new JSONArray(str);
+		session = DBManager.getInstance().getTestSession();
+		int count = 0;
 		for(int i=0;i<arr.length();i++) {
 			JSONObject obj = arr.getJSONObject(i);
 			MovieDTO dto = new MovieDTO(obj.getInt("bno"), obj.getString("title"), obj.getString("open_date"),
 					obj.getInt("audience_count"), obj.getString("director"));
 			System.out.println(dto.toString());
+			// 읽어온 내용을 movie 테이블에 추가
+			count += session.insert("insertTestMovie", dto);
 		}
-		// 읽어온 내용을 movie 테이블에 추가
-		
+		System.out.println("테스트 데이터 " + count + "건 추가 하였습니다.");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		session.rollback();
+		System.out.println("테스트 데이터 제거");
 	}
 
 	@Test

de7db05d36d49b68fbd4acfcb844a8598a50bc62
ITSC <leedh7785@naver.com>
2022-07-20 15:43:12 +0900

    commit

---
+++ BoardFinal/src/main/java/mapper/BoardMapper.java
@@ -0,0 +1,37 @@
+package mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import config.DBManager;
+import dto.BoardDTO;
+
+public class BoardMapper {
+	private static BoardMapper instance = new BoardMapper();
+
+	private BoardMapper() {
+	}
+
+	public static BoardMapper getInstance() {
+		if(instance == null)
+			instance = new BoardMapper();
+		return instance;
+	}
+
+	public List<BoardDTO> selectBoardList(int pageNo) {
+		SqlSession session = DBManager.getInstance().getSession();
+		List<BoardDTO> list = session.selectList("selectBoardList", pageNo);
+		session.close();
+		return list;
+	}
+
+	public BoardDTO selectBoard(int bno) {
+		SqlSession session = DBManager.getInstance().getSession();
+		BoardDTO dto = session.selectOne("selectBoard", bno);
+		return dto;
+	}
+
+	
+}

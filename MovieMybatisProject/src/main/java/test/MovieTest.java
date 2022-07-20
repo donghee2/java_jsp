15ce8514416fe5ccefbc90bf3754b60068645b00
ITSC <leedh7785@naver.com>
2022-07-20 10:49:09 +0900

    commit

---
+++ MovieMybatisProject/src/main/java/test/MovieTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class MovieTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		// movie.json 파일 읽어옴
+		// 읽어온 내용을 movie 테이블에 추가
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
